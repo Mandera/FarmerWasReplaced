37098fd449fef0a427d50e7d18d801b1361c958d
Mandera <rickard.abraham@gmail.com>
2024-06-16 14:48:14 +0300

    Maze single branch exhaustion done

--- maze.py
+++ maze.py
@@ -9,6 +9,7 @@ def start_maze():
     while get_entity_type() == Entities.Bush:
         use_item(Items.Fertilizer)
 
+
 def blank_grid():
     grid = {}
     for x in range(size):
@@ -17,9 +18,11 @@ def blank_grid():
             grid[x][y] = {}
     return grid
 
+
 def update_pos(pos, dir_):
     numbers = direction_numbers[dir_]
     pos[numbers[0]] = (pos[numbers[0]] + numbers[1]) % size
+
 
 def maze(laps):
     for lap in range(laps):
@@ -29,21 +32,26 @@ def maze(laps):
         grid = blank_grid()
 
         start_maze()
-
         while True:
             for dir_i in range(2):
                 directions = direction_indexes[dir_i]
-                result = None
+                do_next_square = False
                 for dir_ in directions:
+                    grid_pos = grid[pos[0]][pos[1]]
 
+                    # Already tried going through here
+                    if dir_ in grid_pos:
+                        continue
 
-                    result = move(dir_)
-                    grid[pos[0]][pos[1]][dir_] = result
-                    if result:
+                    grid_pos[dir_] = move(dir_)
+
+                    # Successfully moved through new
+                    if grid_pos[dir_]:
                         update_pos(pos, dir_)
                         grid[pos[0]][pos[1]][direction_opposite[dir_]] = True
+                        do_next_square = True
                         break
-                if result:
+                if do_next_square:
                     break
 
 
